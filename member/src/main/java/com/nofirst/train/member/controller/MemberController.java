82dba48df2b48afafc9e2660f6de4f5dd8a45667
meiyiming <1025926969@qq.com>
2023-09-30 15:34:11 +0800

    4.19 finished 校验框架

--- member/src/main/java/com/nofirst/train/member/controller/MemberController.java
+++ member/src/main/java/com/nofirst/train/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.nofirst.train.common.resp.CommonResp;
 import com.nofirst.train.member.req.MemberRegisterReq;
 import com.nofirst.train.member.service.MemberService;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,7 +22,7 @@ public class MemberController {
     }
 
     @PostMapping("register")
-    public CommonResp<Long> register(MemberRegisterReq req){
+    public CommonResp<Long> register(@Valid MemberRegisterReq req){
         long userId = memberService.registerUser(req);
 
         return new CommonResp<>(userId);
