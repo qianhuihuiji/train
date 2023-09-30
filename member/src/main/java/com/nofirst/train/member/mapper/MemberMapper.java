2a7caf9d5c210d6444ffb5261a01b5a578a1ff85
meiyiming <1025926969@qq.com>
2023-09-30 13:57:46 +0800

    4.14 finished 集成 mybatis-generator

--- member/src/main/java/com/nofirst/train/member/mapper/MemberMapper.java
+++ member/src/main/java/com/nofirst/train/member/mapper/MemberMapper.java
@@ -1,6 +1,30 @@
 package com.nofirst.train.member.mapper;
 
-public interface MemberMapper {
+import com.nofirst.train.member.domain.Member;
+import com.nofirst.train.member.domain.MemberExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
-    int count();
+public interface MemberMapper {
+    long countByExample(MemberExample example);
+
+    int deleteByExample(MemberExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Member record);
+
+    int insertSelective(Member record);
+
+    List<Member> selectByExample(MemberExample example);
+
+    Member selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByExample(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByPrimaryKeySelective(Member record);
+
+    int updateByPrimaryKey(Member record);
 }
